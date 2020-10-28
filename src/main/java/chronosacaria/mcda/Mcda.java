bac318e5786a782ee6b669e17061d224edc06a95
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2020-10-28 03:57:07 -0400

    Initial commit

---
+++ src/main/java/chronosacaria/mcda/Mcda.java
@@ -0,0 +1,10 @@
+package chronosacaria.mcda;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mcda implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/chronosacaria/mcda/client/McdaClient.java
@@ -0,0 +1,13 @@
+package chronosacaria.mcda.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class McdaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
