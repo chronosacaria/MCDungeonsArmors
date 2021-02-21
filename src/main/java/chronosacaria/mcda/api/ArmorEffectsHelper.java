c4ee07938abd18380ee0e0d983d55008fb0f83ea
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-02-21 16:02:47 -0500

    clean up

--- src/main/java/chronosacaria/mcda/api/ArmorEffectsHelper.java
+++ src/main/java/chronosacaria/mcda/api/ArmorEffectsHelper.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.FoxEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
