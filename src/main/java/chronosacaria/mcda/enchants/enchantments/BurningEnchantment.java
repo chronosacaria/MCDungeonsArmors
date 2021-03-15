dd5eef583fdcb9fbf0336ea2c48714356ced87e3
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-03-15 12:30:26 -0400

    Made BurningEnchantment more like Thorns (it doesn't trigger every time)

--- src/main/java/chronosacaria/mcda/enchants/enchantments/BurningEnchantment.java
+++ src/main/java/chronosacaria/mcda/enchants/enchantments/BurningEnchantment.java
@@ -2,8 +2,14 @@ package chronosacaria.mcda.enchants.enchantments;
 
 import chronosacaria.mcda.enchants.ArmorEnchantment;
 import chronosacaria.mcda.enchants.EnchantID;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+
+import java.util.Map.Entry;
+import java.util.Random;
 
 public class BurningEnchantment extends ArmorEnchantment {
 
@@ -13,10 +19,22 @@ public class BurningEnchantment extends ArmorEnchantment {
 
     @Override
     public void onUserDamaged(LivingEntity user, Entity attacker, int level) {
-        if (attacker != null) {
-            if (!attacker.isOnFire()) {
-                attacker.setOnFireFor(3 * level);
+        Random random = user.getRandom();
+        Entry<EquipmentSlot, ItemStack> entry = EnchantmentHelper.chooseEquipmentWith(BurningEnchantment.this, user);
+        if (shouldDamageAttacker(level, random)) {
+            if (attacker != null) {
+                if (!attacker.isOnFire()) {
+                    attacker.setOnFireFor(3 * level);
+                }
             }
+        }
+    }
+
+    public static boolean shouldDamageAttacker(int level, Random random){
+        if (level <= 0){
+            return false;
+        } else {
+            return random.nextFloat() < 0.15F * (float) level;
         }
     }
 }
