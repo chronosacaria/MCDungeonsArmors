9686fd6d1575362420b72b8d71a5c54cf620fbca
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-03-22 11:22:21 -0400

    Made it so that Bag of Souls stacks

--- src/main/java/chronosacaria/mcda/mixin/ExperienceOrbEntityMixin.java
+++ src/main/java/chronosacaria/mcda/mixin/ExperienceOrbEntityMixin.java
@@ -5,8 +5,10 @@ import chronosacaria.mcda.registry.EnchantsRegistry;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,9 +35,27 @@ public abstract class ExperienceOrbEntityMixin extends Entity {
 
         if (!world.isClient){
             int bagOfSoulsLevel = EnchantmentHelper.getEquipmentLevel(EnchantsRegistry.enchants.get(EnchantID.BAG_OF_SOULS), playerEntity);
+            int bagOfSoulsCount = 0;
+
+            ItemStack helmetStack = playerEntity.getEquippedStack(EquipmentSlot.HEAD);
+            ItemStack chestStack = playerEntity.getEquippedStack(EquipmentSlot.CHEST);
+            ItemStack legsStack = playerEntity.getEquippedStack(EquipmentSlot.LEGS);
+            ItemStack feetStack = playerEntity.getEquippedStack(EquipmentSlot.FEET);
 
             if (bagOfSoulsLevel > 0) {
-                this.amount = this.amount * (1 + (bagOfSoulsLevel / 3));
+                if (EnchantmentHelper.getLevel(EnchantsRegistry.enchants.get(BAG_OF_SOULS), helmetStack) > 0) {
+                    bagOfSoulsCount ++;
+                }
+                if (EnchantmentHelper.getLevel(EnchantsRegistry.enchants.get(BAG_OF_SOULS), chestStack) > 0) {
+                    bagOfSoulsCount ++;
+                }
+                if (EnchantmentHelper.getLevel(EnchantsRegistry.enchants.get(BAG_OF_SOULS), legsStack) > 0) {
+                    bagOfSoulsCount ++;
+                }
+                if (EnchantmentHelper.getLevel(EnchantsRegistry.enchants.get(BAG_OF_SOULS), feetStack) > 0) {
+                    bagOfSoulsCount ++;
+                }
+                this.amount = (this.amount * (1 + (bagOfSoulsLevel / 3))) * bagOfSoulsCount;
                 this.remove();
             }
         }
