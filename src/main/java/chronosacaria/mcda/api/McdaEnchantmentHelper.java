d3b195b6294ff7d34eb3a6ec3209dbf39648e3f2
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-12-19 01:07:10 -0500

    Vastly improved on Bag of Soul's Code

--- src/main/java/chronosacaria/mcda/api/McdaEnchantmentHelper.java
+++ src/main/java/chronosacaria/mcda/api/McdaEnchantmentHelper.java
@@ -1,12 +1,35 @@
 package chronosacaria.mcda.api;
 
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+
+import java.util.Iterator;
 
 public class McdaEnchantmentHelper {
 
     public static boolean hasFireAspect(LivingEntity entity) {
         return EnchantmentHelper.getEquipmentLevel(Enchantments.FIRE_ASPECT, entity) > 0;
+    }
+
+    public static int getBagOfSoulsLevel(Enchantment enchantment, PlayerEntity entity){
+        Iterable<ItemStack> iterable = enchantment.getEquipment(entity).values();
+        if (iterable == null) {
+            return 0;
+        } else {
+            int i = 0;
+            Iterator var4 = iterable.iterator();
+
+            while(var4.hasNext()) {
+                ItemStack itemStack = (ItemStack)var4.next();
+                int j = EnchantmentHelper.getLevel(enchantment, itemStack);
+                i += j;
+            }
+
+            return i;
+        }
     }
 }
--- src/main/java/chronosacaria/mcda/mixin/ExperienceOrbEntityMixin.java
+++ src/main/java/chronosacaria/mcda/mixin/ExperienceOrbEntityMixin.java
@@ -1,12 +1,11 @@
 package chronosacaria.mcda.mixin;
 
+import chronosacaria.mcda.api.McdaEnchantmentHelper;
 import chronosacaria.mcda.enchants.EnchantID;
 import chronosacaria.mcda.registry.EnchantsRegistry;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.ExperienceOrbEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -15,6 +14,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Iterator;
+import java.util.Random;
 
 import static chronosacaria.mcda.config.McdaConfig.config;
 import static chronosacaria.mcda.enchants.EnchantID.*;
@@ -34,7 +36,9 @@ public abstract class ExperienceOrbEntityMixin extends Entity {
             return;
 
         if (!world.isClient){
-            int bagOfSoulsLevel = EnchantmentHelper.getEquipmentLevel(EnchantsRegistry.enchants.get(EnchantID.BAG_OF_SOULS), playerEntity);
+            int bagOfSoulsLevel =
+                    McdaEnchantmentHelper.getBagOfSoulsLevel(EnchantsRegistry.enchants.get(EnchantID.BAG_OF_SOULS),
+                    playerEntity);
             int bagOfSoulsCount = 0;
 
             ItemStack helmetStack = playerEntity.getEquippedStack(EquipmentSlot.HEAD);
@@ -55,7 +59,9 @@ public abstract class ExperienceOrbEntityMixin extends Entity {
                 if (EnchantmentHelper.getLevel(EnchantsRegistry.enchants.get(BAG_OF_SOULS), feetStack) > 0) {
                     bagOfSoulsCount ++;
                 }
-                this.amount = (this.amount * (1 + (bagOfSoulsLevel / 3))) * bagOfSoulsCount;
+
+                // Thank you, Amph
+                this.amount = (this.amount * (1 + (bagOfSoulsLevel / 12)) + Math.round(((bagOfSoulsLevel % 12)/12.0f) * this.amount)) * bagOfSoulsCount;
                 this.remove(RemovalReason.KILLED);
             }
         }
