1659593d0a8749dffd8e6465ca5dbe881b249a4d
Kluzzio <kluzzio.amp@gmail.com>
2022-01-25 11:40:03 -0800

    Cleanup AbilityHelper  - Removed unused things  - uncommented test change

--- src/main/java/chronosacaria/mcda/api/AbilityHelper.java
+++ src/main/java/chronosacaria/mcda/api/AbilityHelper.java
@@ -1,8 +1,6 @@
 package chronosacaria.mcda.api;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.TargetPredicate;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Box;
@@ -11,24 +9,6 @@ import java.util.List;
 
 public class AbilityHelper {
 
-    // TODO: unused
-    public static void makeNearbyPetsAttackTarget(LivingEntity target, LivingEntity owner) {
-        List<MobEntity> nearbyPets = owner.getEntityWorld().getEntitiesByClass(MobEntity.class,
-                new Box(owner.getX() - 12, owner.getY() - 12, owner.getZ() - 12,
-                        owner.getX() + 12, owner.getY() + 12, owner.getZ() + 12),
-                (nearbyEntity) -> nearbyEntity != owner && isPetOf(nearbyEntity, owner) && nearbyEntity.isAlive());
-
-        for (MobEntity nearbyPet : nearbyPets) {
-            nearbyPet.setTarget(target);
-        }
-    }
-
-    public static LivingEntity getClosestPounceTarget(LivingEntity pouncer, LivingEntity targets, LivingEntity closest, float distance){
-        return targets.getEntityWorld().getClosestEntity(getPotentialPounceTargets(pouncer, distance),
-                TargetPredicate.DEFAULT,
-                closest, closest.getX(), closest.getY(), closest.getZ());
-    }
-
     public static List<LivingEntity> getPotentialPounceTargets(LivingEntity pouncer, float distance){
         return pouncer.getEntityWorld().getEntitiesByClass(LivingEntity.class,
                 new Box(pouncer.getBlockPos()).expand(distance),
@@ -36,30 +16,8 @@ public class AbilityHelper {
     }
 
     public static boolean isPetOf(LivingEntity self, LivingEntity owner) {
-        if (self instanceof TameableEntity) {
-            TameableEntity pet = (TameableEntity) self;
+        if (self instanceof TameableEntity pet)
             return pet.getOwner() == owner;
-        }
-        /*if (self instanceof HorseBaseEntity) {
-            HorseBaseEntity abstractHorse = (HorseBaseEntity) self;
-            //return GoalUtils.getOwner(abstractHorse) == owner;
-        }
-        if (self instanceof IronGolemEntity) {
-            IronGolemEntity ironGolem = (IronGolemEntity) self;
-            //return GoalUtils.getOwner(ironGolem) == owner;
-        }
-        if (self instanceof BatEntity) {
-            BatEntity batEntity = (BatEntity) self;
-            //return GoalUtils.getOwner(batEntity) == owner;
-        }
-        if (self instanceof BeeEntity) {
-            BeeEntity beeEntity = (BeeEntity) self;
-            //return GoalUtils.getOwner(beeEntity) == owner;
-        }
-        if (self instanceof SheepEntity) {
-            SheepEntity sheepEntity = (SheepEntity) self;
-            //return GoalUtils.getOwner(sheepEntity) == owner;
-        }*/
         return false;
     }
 
@@ -88,7 +46,7 @@ public class AbilityHelper {
     public static boolean isPounceTarget(LivingEntity self, LivingEntity attacker, LivingEntity center){
         return self != attacker
                 && self.isAlive()
-                //&& !isAllyOf(attacker, self)
+                && !isAllyOf(attacker, self)
                 && center.canSee(self);
     }
 
